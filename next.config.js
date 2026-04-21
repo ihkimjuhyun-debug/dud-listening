1874d47445de86604a2366ed6f3b10e9cad8cbe3
ihkimjuhyun-debug <ihkimjuhyun@gmail.com>
2026-04-21 12:08:16 +0900

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  serverRuntimeConfig: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    ADMIN_PIN: process.env.ADMIN_PIN,
-  },
-  publicRuntimeConfig: {},
-};
